5faf1e060d52163c71cf9c56024bd4cefd927a26
ssabrut <michael.gunawan2002@gmail.com>
2024-07-15 08:23:22 +0700

    Initial commit

---
+++ bubadibaco/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  bubadibaco
+//
+//  Created by Michael Eko on 15/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
