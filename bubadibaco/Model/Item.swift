110328cc81c49c47a5a048032de70bdc7b187149
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2024-07-15 11:03:36 +0700

    Add Task Model
    Name: String
    isDone: Bool

---
+++ bubadibaco/Model/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  bubadibaco
+//
+//  Created by Evelyn Callista Yaurentius on 15/07/24.
+//
+
+import Foundation
---
+++ bubadibaco/ViewModel/PencilBoardViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PencilViewModel.swift
+//  bubadibaco
+//
+//  Created by Evelyn Callista Yaurentius on 15/07/24.
+//
+
+import Foundation
+import PencilKit
+import UIKit
+
+class PencilBoardViewModel: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
