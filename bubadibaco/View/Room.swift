25e1f035bf8d51bfaccb427b55795a81eb398f62
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2024-07-18 08:58:31 +0700

    FIx Task & Item implementation
    - RoomDataController store all the object data
    - Add sound to item model

--- bubadibaco/View/Room.swift
+++ bubadibaco/View/Room.swift
@@ -9,77 +9,62 @@ import SwiftUI
 import AVFoundation
 
 struct Room: View {
+    @ObservedObject var roomData: RoomData
     @State private var objectClicked: String?
     @State private var audioPlayer: AVAudioPlayer?
     @State private var isShowingAlphabets = false
 
     var body: some View {
-        NavigationView {
-            ScrollView(.horizontal) {
-                ZStack {
-                    Image("bgRoom")
+            NavigationView {
+                ScrollView(.horizontal) {
+                    ZStack {
+                        Image("bgRoom")
 
-                    Image("ball")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 150, height: 150)
-                        .offset(x:100, y:300)
-                        .onTapGesture {
-                            objectClicked = "ball"
-                            playSound(named: "Ballsound")
-                            isShowingAlphabets = true
+                        ForEach(items, id: \.self) { item in
+                            Image(item.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 150, height: 150)
+                                .offset(getOffset(for: item))
+                                .onTapGesture {
+                                    objectClicked = item.name
+                                    playSound(named: "\(item.name)Sound")
+                                    isShowingAlphabets = true
+                                }
                         }
-                    
-                    Image("cake")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 150)
-                        .offset(x:1800, y:0)
-                        .onTapGesture {
-                            objectClicked = "cake"
-                            playSound(named: "Cakesound")
-                            isShowingAlphabets = true
-                        }
-                    
-                    Image("milk")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 250, height: 150)
-                        .offset(x:-1150, y: 30)
-                        .onTapGesture {
-                            objectClicked = "milk"
-                            playSound(named: "MilkSound")
-                            isShowingAlphabets = true
-                        }
-                    
-                    Image("bed")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 900)
-                        .offset(x:-2150, y: 100)
-                        .onTapGesture {
-                            objectClicked = "bed"
-                            playSound(named: "BedSound")
-                            isShowingAlphabets = true
-                        }
+                    }
+                    .navigationBarHidden(true)
+                    .navigationViewStyle(StackNavigationViewStyle())
+                    .background(
+                        NavigationLink(
+                            destination: Alphabets(objectName: objectClicked ?? ""),
+                            isActive: $isShowingAlphabets,
+                            label: { EmptyView() }
+                        )
+                    )
                 }
                 .navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.all)
                 .navigationViewStyle(StackNavigationViewStyle())
-                .background(
-                    NavigationLink(
-                        destination: Alphabets(objectName: objectClicked ?? ""),
-                        isActive: $isShowingAlphabets,
-                        label: { EmptyView() }
-                    )
-                )
             }
             .navigationBarHidden(true)
-            .edgesIgnoringSafeArea(.all)
             .navigationViewStyle(StackNavigationViewStyle())
         }
-        .navigationBarHidden(true)
-        .navigationViewStyle(StackNavigationViewStyle())
-    }
+    
+    func getOffset(for item: Item) -> CGSize {
+            switch item.name {
+            case "ball":
+                return CGSize(width: 100, height: 300)
+            case "cake":
+                return CGSize(width: 1800, height: 0)
+            case "milk":
+                return CGSize(width: -1150, height: 30)
+            case "bed":
+                return CGSize(width: -2150, height: 100)
+            default:
+                return CGSize.zero
+            }
+        }
 
     func playSound(named name: String) {
         guard let url = Bundle.main.url(forResource: name, withExtension: "m4a") else {
@@ -96,6 +81,3 @@ struct Room: View {
     }
 }
 
-#Preview {
-    Room()
-}
