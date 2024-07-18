afbd3cfd6380845cb457f66b8d6f031bc13e925d
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2024-07-18 08:14:14 +0700

    Task and Item implemented

--- bubadibaco/Model/Item.swift
+++ bubadibaco/Model/Item.swift
@@ -12,3 +12,18 @@ struct Item: Hashable{
     var image : String
     var type : Task
 }
+
+let items = [
+    Item(name: "Cake", image: "cake_image", type: tasks[0]),
+    Item(name: "Beef", image: "beef_image", type: tasks[0]),
+    Item(name: "Corn", image: "corn_image", type: tasks[0]),
+    Item(name: "Milk", image: "milk_image", type: tasks[1]),
+    Item(name: "Soda", image: "soda_image", type: tasks[1]),
+    Item(name: "Tea", image: "tea_image", type: tasks[1]),
+    Item(name: "Ball", image: "ball_image", type: tasks[2]),
+    Item(name: "Doll", image: "doll_image", type: tasks[2]),
+    Item(name: "Card", image: "card_image", type: tasks[2]),
+    Item(name: "Bed", image: "bed_image", type: tasks[3]),
+    Item(name: "Sofa", image: "sofa_image", type: tasks[3]),
+    Item(name: "Tent", image: "tent_image", type: tasks[3]),
+]
--- bubadibaco/Model/Task.swift
+++ bubadibaco/Model/Task.swift
@@ -11,3 +11,10 @@ struct Task: Hashable {
     var name : String
     var isDone : Bool
 }
+
+let tasks = [
+    Task(name: "Eat", isDone: false),
+    Task(name: "Drink", isDone: false),
+    Task(name: "Play", isDone: false),
+    Task(name: "Sleep", isDone: false)
+]
