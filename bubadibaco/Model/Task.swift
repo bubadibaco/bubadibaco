7c5ea24635d0be7e03fd74c69e8d73cccf66262c
Michael Eco <michael.gunawan2002@gmail.com>
2024-07-19 07:59:22 +0700

    Revert "Revisi todo"

--- bubadibaco/Model/Task.swift
+++ bubadibaco/Model/Task.swift
@@ -7,15 +7,7 @@
 
 import Foundation
 
-struct Task: Hashable, Identifiable {
-    var id = UUID()
-    var name: String
-    var isDone: Bool
+struct Task: Hashable {
+    var name : String
+    var isDone : Bool
 }
-
-let tasks = [
-    Task(name: "Eat", isDone: false),
-    Task(name: "Drink", isDone: false),
-    Task(name: "Play", isDone: false),
-    Task(name: "Sleep", isDone: false)
-]
