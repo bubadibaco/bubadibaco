1748a4e6c6a4e1d25f2f33c48c84615728b06287
9wynneth <gwynnethgwen10@gmail.com>
2024-07-20 22:36:06 +0700

    bg ws diganti, objects position changed, sounds for objects done

---
+++ bubadibaco/ViewModel/AudioPlayerHelper.swift
@@ -0,0 +1,34 @@
+//
+//  AudioPlayerHelper.swift
+//  bubadibaco
+//
+//  Created by Gwynneth Isviandhy on 20/07/24.
+//
+
+import SwiftUI
+import AVFoundation
+
+class AudioPlayerHelper: NSObject, AVAudioPlayerDelegate {
+    private var audioPlayer: AVAudioPlayer?
+    private var completion: (() -> Void)?
+
+    func playSound(named name: String, completion: (() -> Void)? = nil) {
+        guard let dataAsset = NSDataAsset(name: name) else {
+            print("Could not find the sound asset for \(name).")
+            return
+        }
+        do {
+            self.completion = completion
+            audioPlayer = try AVAudioPlayer(data: dataAsset.data)
+            audioPlayer?.delegate = self
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the sound file for \(name). Error: \(error.localizedDescription)")
+        }
+    }
+
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        completion?()
+    }
+}
+
