67a667ce0035a12fdf8551aceeea153e4715d4b7
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2024-07-23 09:32:29 +0700

    Implement animation
    - Character tapped 2 times to confirm, first tap start animation
    - Adjust size on character intro
    - Animate object

--- bubadibaco/View/Room.swift
+++ bubadibaco/View/Room.swift
@@ -66,6 +66,7 @@ struct Room: View {
                                     .scaledToFit()
                                     .frame(width: frameSizes[item.name]?.width, height: frameSizes[item.name]?.height)
                                     .offset(x: itemOffsets[item.name]?.x ?? 0, y: itemOffsets[item.name]?.y ?? 0)
+                                    .offsetWiggle()
                                     .onTapGesture {
                                         objectClicked = item.name
                                         
@@ -78,6 +79,7 @@ struct Room: View {
                                             isShowingAlphabets = true
                                         }
                                     }
+                                    
                                 
                             }
                             
