2d5c911ab870af3ca771c7e04b305273aef03ded
9wynneth <gwynnethgwen10@gmail.com>
2024-07-24 09:32:45 +0700

    todoview done

--- bubadibaco/View/ToDo.swift
+++ bubadibaco/View/ToDo.swift
@@ -9,20 +9,77 @@ import SwiftUI
 
 struct Todo: View {
     var body: some View {
-        List {
-            ForEach(tasks) { task in
-                HStack {
-                    if task.isDone {
-                        Text(task.name).strikethrough()
+        
+        ZStack {
+            ScrollView {
+                ZStack {
+                    Image("bgTodo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity, maxHeight: 600)
+                        .clipped()
+                    
+                    VStack(spacing: 10) {
+                            ForEach(tasks) {task in
+                                
+                                HStack {
+                                    if task.isDone {
+                                        Image("\(task.name)Yellow")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 160, height: 50)
+                                            .clipped()
+                                    }
+                                    else {
+                                        Image("\(task.name)Green")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 160, height: 50)
+                                            .clipped()
+
+                                    }
+                                }
+                                
+                                
+                            }
+                            
+//                            
+//                            Image("EatYellow")
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fill)
+//                                .frame(width: 160, height: 50)
+//                                .clipped()
+                        
                     }
-                    else {
-                        Text(task.name)
-                    }
-                }
-                .padding()
+                    .padding(.top, 10)
+                }.frame(height: 300)
             }
         }
         .frame(width: 200, height: 200)
+        //        .frame(width: 200, height: 200)
+        
+        //        List {
+        //            ForEach(tasks) { task in
+        //
+        //
+        //                HStack {
+        //                    if task.isDone {
+        //
+        //
+        //                        Text(task.name).strikethrough()
+        //                    }
+        //                    else {
+        //                        Text(task.name)
+        //                    }
+        //                }
+        //                .padding()
+        //
+        //
+        //
+        //
+        //            }
+        //        }
+        //        .frame(width: 200, height: 200)
     }
 }
 
