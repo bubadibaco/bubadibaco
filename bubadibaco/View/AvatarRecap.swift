860bfc86c06e85748e6db1a78e512763ee3a3d07
Pelangi Masita <pelangimasita18@gmail.com>
2024-07-25 09:56:10 +0700

    Add sleep view

--- bubadibaco/View/AvatarRecap.swift
+++ bubadibaco/View/AvatarRecap.swift
@@ -1,4 +1,3 @@
-//
 //  AvatarRecap.swift
 //  bubadibaco
 //
@@ -16,74 +15,81 @@ struct AvatarRecap: View {
     var selectedObjects: [String: String]
     @Binding var stories: [Story]
     @State private var hasSpoken = false
-    @State private var navigateToGameViewController = false
+    @State private var navigateToSleep = false
 
     var body: some View {
-        ZStack {
-            Image("bgRoom")
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                ZStack {
-                    Image("speechBubble")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 600, height: 200)
-                    VStack {
-                        Text("Yay, you've done a good job in helping \(selectedAvatar) to eat \(selectedObjects["Eat"] ?? "eat"), drink some \(selectedObjects["Drink"] ?? "drink"), play \(selectedObjects["Play"] ?? "play"), and finally went to sleep in \(selectedObjects["Sleep"] ?? "sleep"). See you tomorrow!")
-                            .frame(width: 350, height: 180)
+        NavigationView {
+            ZStack {
+                Image("bgRoom")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    ZStack {
+                        Image("speechBubble")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 600, height: 200)
+                        VStack {
+                            Text("Yay, you've done a good job in helping \(selectedAvatar) to eat \(selectedObjects["Eat"] ?? "eat"), drink some \(selectedObjects["Drink"] ?? "drink"), play \(selectedObjects["Play"] ?? "play"), and finally went to sleep in \(selectedObjects["Sleep"] ?? "sleep"). See you tomorrow!")
+                                .frame(width: 350, height: 180)
+                                .padding()
+                        }
+                    }
+                    .onAppear {
+                        if !hasSpoken {
+                            speakMessage()
+                            hasSpoken = true
+                        }
+                    }
+                    
+                    if selectedAvatar == "Terry" {
+                        Image("dino")
+                            .resizable()
+                            .scaledToFit()
                             .padding()
+                            .frame(maxWidth: 400)
+                            .onAppear {
+                                print("Displaying image for Terry (dino)")
+                            }
+                            .onTapGesture {
+                                speakMessage()
+                            }
+                    } else if selectedAvatar == "Trixie" {
+                        Image("unicorn")
+                            .resizable()
+                            .scaledToFit()
+                            .padding()
+                            .frame(maxWidth: 400)
+                            .onAppear {
+                                print("Displaying image for \(character.name) (\(character.image))")
+                            }
+                            .onTapGesture {
+                                speakMessage()
+                            }
                     }
-                }
-                .onAppear {
-                    if !hasSpoken {
-                        speakMessage()
-                        hasSpoken = true
+                    
+                    NavigationLink(
+                        destination: SleepView(selectedAvatar: selectedAvatar),
+                        isActive: $navigateToSleep
+                    ) {
+                        EmptyView()
                     }
-                }
-                
-                if selectedAvatar == "Terry" {
-                    Image("dino")
-                        .resizable()
-                        .scaledToFit()
-                        .padding()
-                        .frame(maxWidth: 400)
-                        .onAppear {
-                            print("Displaying image for Terry (dino)")
-                        }
-                        .onTapGesture {
-                            speakMessage()
-                        }
-                } else if selectedAvatar == "Trixie" {
-                    Image("unicorn")
-                        .resizable()
-                        .scaledToFit()
-                        .padding()
-                        .frame(maxWidth: 400)
-                        .onAppear {
-                            print("Displaying image for \(character.name) (\(character.image))")
-                        }
-                        .onTapGesture {
-                            speakMessage()
-                        }
-                }
-                
-                NavigationLink(destination: GameViewControllerWrapper(), isActive: $navigateToGameViewController) {
-                    EmptyView()
-                }
-                
-                Button(action: finishStory) {
-                    Text("Finish")
-                        .font(.title)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+
+                    Button(action: goToSleep) {
+                        Text("Sleep")
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
                 }
             }
         }
+        .navigationBarHidden(true)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     private func speakMessage() {
@@ -94,25 +100,7 @@ struct AvatarRecap: View {
         synthesizer.speak(utterance)
     }
     
-    private func finishStory() {
-        if let index = stories.firstIndex(where: { $0.name == "Terry and Trixie" }) {
-            stories[index].isUnlocked = true
-            if index + 1 < stories.count {
-                stories[index + 1].isUnlocked = true
-            }
-        }
-        navigateToGameViewController = true
-    }
-}
-
-struct GameViewControllerWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> GameViewController {
-        return GameViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: GameViewController, context: Context) {
-        if let navigationController = uiViewController.navigationController {
-            navigationController.setNavigationBarHidden(true, animated: false)
-        }
+    private func goToSleep() {
+        navigateToSleep = true
     }
 }
